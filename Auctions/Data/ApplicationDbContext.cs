c309efd3e223d132ae7a92fbaf702e297c346a6a
garizola <99741841+garizola@users.noreply.github.com>
2024-03-22 13:47:09 -0400

    perms to work

--- Auctions/Data/ApplicationDbContext.cs
+++ Auctions/Data/ApplicationDbContext.cs
@@ -2,14 +2,17 @@
 using System.Security.Cryptography;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Data.SqlClient;
 
 namespace Auctions.Data;
 
 public class ApplicationDbContext : IdentityDbContext
 {
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, IHttpContextAccessor accessor)
         : base(options)
     {
+        var conn = Database.GetDbConnection() as SqlConnection;
+        conn.AccessToken = accessor.HttpContext.Request.Headers["X-MS-TOKEN-AAD-ACCESS-TOKEN"];
     }
 
     public DbSet<Listing> Listings { get; set; }
